931df6edf6c7f4effa866382bce9f2dac31d2d20
Charlie Knudsen <charlie.knudsen@gmail.com>
2016-11-26 15:44:40 -0600

    Test out deployment

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -eux
 
-sudo /opt/google-cloud-sdk/bin/gcloud docker push gcr.io/${PROJECT_NAME}/${APP_NAME}
-#sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
-#kubectl patch deployment docker-hello-google -p '{"spec":{"template":{"spec":{"containers":[{"name":"docker-hello-google","image":"us.gcr.io/circle-ctl-test/hello:'"$CIRCLE_SHA1"'"}]}}}}'
+sudo /opt/google-cloud-sdk/bin/gcloud docker -- push gcr.io/${PROJECT_NAME}/${APP_NAME}
+
+sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
+# export GOOGLE_APPLICATION_CREDENTIALS=/home/ubuntu/account-auth.json
+# kubectl patch deployment circle-gke-test -p '{"spec":{"template":{"spec":{"containers":[{"name":"docker-hello-google","image":"gcr.io/charliek-apps/circle-gke-test:'"$CIRCLE_SHA1"'"}]}}}}'
+kubectl set image deployment/circle-gke-test circle-gke-test=gcr.io/charliek-apps/${APP_NAME}:$CIRCLE_SHA1
