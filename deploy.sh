9e21e0a59e6a1d0aac9c2542d6b69c2bd08a9b9b
Charlie Knudsen <charlie.knudsen@gmail.com>
2016-11-26 15:52:14 -0600

    Continue troubleshooting deploy

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,5 @@
 sudo /opt/google-cloud-sdk/bin/gcloud docker -- push gcr.io/${PROJECT_NAME}/${APP_NAME}
 
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
-# export GOOGLE_APPLICATION_CREDENTIALS=/home/ubuntu/account-auth.json
-# kubectl patch deployment circle-gke-test -p '{"spec":{"template":{"spec":{"containers":[{"name":"docker-hello-google","image":"gcr.io/charliek-apps/circle-gke-test:'"$CIRCLE_SHA1"'"}]}}}}'
+export GOOGLE_APPLICATION_CREDENTIALS=/home/ubuntu/account-auth.json
 kubectl set image deployment/circle-gke-test circle-gke-test=gcr.io/charliek-apps/${APP_NAME}:$CIRCLE_SHA1
